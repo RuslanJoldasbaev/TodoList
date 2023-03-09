baebe3a682effc0a5f1b07dfda3f66ec0a07771f
RuslanJoldasbaev <joldasbaevruslan4>
2023-03-09 12:43:17 +0500

    SplashFragmentke lottie animation qostim

--- app/src/main/java/com/example/todolist/SplashFragment.kt
+++ app/src/main/java/com/example/todolist/SplashFragment.kt
@@ -1,33 +1,42 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.todolist.data.local.LocalStorage
+import com.example.todolist.databinding.FragmentSplashBinding
 import kotlinx.coroutines.delay
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
-
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding = FragmentSplashBinding.bind(view)
+
         if (LocalStorage().isRegister) {
             lifecycleScope.launchWhenResumed {
-                delay(2000)
-                findNavController().navigate(
-                    SplashFragmentDirections.actionSplashFragmentToMainFragment()
-                )
+                binding.infoLogo.playAnimation()
+                Handler().postDelayed({
+                    findNavController().navigate(
+                        SplashFragmentDirections.actionSplashFragmentToMainFragment()
+                    )
+                }, 2000)
             }
         } else {
-           lifecycleScope.launchWhenResumed {
-               delay(2000)
-               findNavController().navigate(
-                   SplashFragmentDirections.actionSplashFragmentToLoginFragment()
-               )
-           }
+            lifecycleScope.launchWhenResumed {
+                binding.infoLogo.playAnimation()
+                findNavController().navigate(
+                    SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+                )
+                Handler().postDelayed({
+                }, 2000)
+            }
         }
     }
 }
